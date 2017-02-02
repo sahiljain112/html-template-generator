e57304150eafd746d494f9fab647b15c51f754cb
sahiljain112 <sahiljain112@gmail.com>
2017-02-02 15:55:18 +0530

    Added quick template generation

--- spec/html-template-generator-spec.js
+++ spec/html-template-generator-spec.js
@@ -1,73 +1,76 @@
-'use babel';
-// Test cases will be added in release 0.3.0
-import HtmlTemplateGenerator from '../lib/html-template-generator';
-
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+// 'use babel';
+// // Test cases will be added in next release
+// import HtmlTemplateGenerator from '../lib/html-template-generator';
 //
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
-// describe('HtmlTemplateGenerator', () => {
-//   let workspaceElement, activationPromise;
+// // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+// //
+// // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// // or `fdescribe`). Remove the `f` to unfocus the block.
 //
-//   beforeEach(() => {
-//     workspaceElement = atom.views.getView(atom.workspace);
-//     activationPromise = atom.packages.activatePackage('html-template-generator');
-//   });
-//
-//   describe('when the Generate package event is triggered', () => {
-//     it('hides and shows the modal panel with the form generated', () => {
-//       // Before the activation event the view is not on the DOM, and no panel
-//       // has been created
-//       expect(workspaceElement.querySelector('.html-template-generator')).not.toExist();
-//
+// // describe('HtmlTemplateGenerator', () => {
+// //   let workspaceElement, activationPromise;
+// //
+// //   const executeCommand = (callback) => {
+// //     waitsForPromise(() => {
+// //       return activationPromise;
+// //     });
+// //       runs(callback)
+// //   }
+// //
+// //   beforeEach(() => {
+// //     workspaceElement = atom.views.getView(atom.workspace);
+// //     activationPromise = atom.packages.activatePackage('html-template-generator');
+// //   });
+// //
+// //   describe('when the Generate package event is triggered', () => {
+// //     it('hides and shows the modal panel with the form generated', () => {
+// //       // Before the activation event the view is not on the DOM, and no panel
+// //       // has been created
+// //       executeCommand(() => expect(workspaceElement.querySelector('.html-template-generator')).not.toExist())
+// //       atom.commands.dispatch(workspaceElement, 'html-template-generator:generate');
+// //       executeCommand(() => expect(workspaceElement.querySelector('.html-template-generator')).toExist())
 //       // This is an activation event, triggering it will cause the package to be
 //       // activated.
-//       atom.commands.dispatch(workspaceElement, 'html-template-generator:generate');
 //
-//       waitsForPromise(() => {
-//         return activationPromise;
-//       });
-//
-//       runs(() => {
-//         expect(workspaceElement.querySelector('.html-template-generator')).toExist();
-//
-//         let htmlTemplateGeneratorElement = workspaceElement.querySelector('.html-template-generator');
-//         expect(htmlTemplateGeneratorElement).toExist();
-//
-//         let htmlTemplateGeneratorPanel = atom.workspace.panelForItem(htmlTemplateGeneratorElement);
-//         expect(htmlTemplateGeneratorPanel.isVisible()).toBe(true);
-//         atom.commands.dispatch(workspaceElement, 'html-template-generator:toggle');
-//         expect(htmlTemplateGeneratorPanel.isVisible()).toBe(false);
-//       });
+//       // runs(() => {
+//       //   expect(workspaceElement.querySelector('.html-template-generator')).toExist();
+//       //
+//       //   let htmlTemplateGeneratorElement = workspaceElement.querySelector('.html-template-generator');
+//       //   expect(htmlTemplateGeneratorElement).toExist();
+//       //
+//       //   let htmlTemplateGeneratorPanel = atom.workspace.panelForItem(htmlTemplateGeneratorElement);
+//       //   expect(htmlTemplateGeneratorPanel.isVisible()).toBe(true);
+//       //   atom.commands.dispatch(workspaceElement, 'html-template-generator:toggle');
+//       //   expect(htmlTemplateGeneratorPanel.isVisible()).toBe(false);
+//       // });
 //     });
 //
-//     it('hides and shows the view', () => {
-//       // This test shows you an integration test testing at the view level.
-//
-//       // Attaching the workspaceElement to the DOM is required to allow the
-//       // `toBeVisible()` matchers to work. Anything testing visibility or focus
-//       // requires that the workspaceElement is on the DOM. Tests that attach the
-//       // workspaceElement to the DOM are generally slower than those off DOM.
-//       jasmine.attachToDOM(workspaceElement);
-//
-//       expect(workspaceElement.querySelector('.html-template-generator')).not.toExist();
-//
-//       // This is an activation event, triggering it causes the package to be
-//       // activated.
-//       atom.commands.dispatch(workspaceElement, 'html-template-generator:toggle');
-//
-//       waitsForPromise(() => {
-//         return activationPromise;
-//       });
-//
-//       runs(() => {
-//         // Now we can test for view visibility
-//         let htmlTemplateGeneratorElement = workspaceElement.querySelector('.html-template-generator');
-//         expect(htmlTemplateGeneratorElement).toBeVisible();
-//         atom.commands.dispatch(workspaceElement, 'html-template-generator:toggle');
-//         expect(htmlTemplateGeneratorElement).not.toBeVisible();
-//       });
-//     });
+//     // it('hides and shows the view', () => {
+//     //   // This test shows you an integration test testing at the view level.
+//     //
+//     //   // Attaching the workspaceElement to the DOM is required to allow the
+//     //   // `toBeVisible()` matchers to work. Anything testing visibility or focus
+//     //   // requires that the workspaceElement is on the DOM. Tests that attach the
+//     //   // workspaceElement to the DOM are generally slower than those off DOM.
+//     //   jasmine.attachToDOM(workspaceElement);
+//     //
+//     //   expect(workspaceElement.querySelector('.html-template-generator')).not.toExist();
+//     //
+//     //   // This is an activation event, triggering it causes the package to be
+//     //   // activated.
+//     //   atom.commands.dispatch(workspaceElement, 'html-template-generator:toggle');
+//     //
+//     //   waitsForPromise(() => {
+//     //     return activationPromise;
+//     //   });
+//     //
+//     //   runs(() => {
+//     //     // Now we can test for view visibility
+//     //     let htmlTemplateGeneratorElement = workspaceElement.querySelector('.html-template-generator');
+//     //     expect(htmlTemplateGeneratorElement).toBeVisible();
+//     //     atom.commands.dispatch(workspaceElement, 'html-template-generator:toggle');
+//     //     expect(htmlTemplateGeneratorElement).not.toBeVisible();
+//     //   });
+//     // });
 //   });
 // });
